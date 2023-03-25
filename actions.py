12a3c49a9c3fe15da4c04e384dcdceb358c91215
Anish Diwan <56624586+anishhdiwan@users.noreply.github.com>
2023-03-25 16:42:38 +0100

    Add model

---
+++ actions.py
@@ -0,0 +1,164 @@
+# Defining actions as dictionaries (7 actions in total)
+
+# [fwd, bwd, left, right, front-left, front-right, look left, look right, look up, look down, jump, front-jump, attack]
+
+
+actions = [
+
+{
+    "attack": 0,
+    "back": 0,
+    "camera": [0,0],
+    "forward": 1,
+    "jump": 0,
+    "left": 0,
+    "right": 0,
+    "sneak": 0,
+    "sprint": 0
+},
+
+{
+    "attack": 0,
+    "back": 1,
+    "camera": [0,0],
+    "forward": 0,
+    "jump": 0,
+    "left": 0,
+    "right": 0,
+    "sneak": 0,
+    "sprint": 0
+},
+
+{
+    "attack": 0,
+    "back": 0,
+    "camera": [0,0],
+    "forward": 0,
+    "jump": 0,
+    "left": 1,
+    "right": 0,
+    "sneak": 0,
+    "sprint": 0
+},
+
+{
+    "attack": 0,
+    "back": 0,
+    "camera": [0,0],
+    "forward": 0,
+    "jump": 0,
+    "left": 0,
+    "right": 1,
+    "sneak": 0,
+    "sprint": 0
+},
+
+{
+    "attack": 0,
+    "back": 0,
+    "camera": [0,0],
+    "forward": 1,
+    "jump": 0,
+    "left": 1,
+    "right": 0,
+    "sneak": 0,
+    "sprint": 0
+},
+
+{
+    "attack": 0,
+    "back": 0,
+    "camera": [0,0],
+    "forward": 1,
+    "jump": 0,
+    "left": 0,
+    "right": 1,
+    "sneak": 0,
+    "sprint": 0
+},
+
+{
+    "attack": 0,
+    "back": 0,
+    "camera": [2,0],
+    "forward": 0,
+    "jump": 0,
+    "left": 0,
+    "right": 0,
+    "sneak": 0,
+    "sprint": 0
+},
+
+{
+    "attack": 0,
+    "back": 0,
+    "camera": [-2,0],
+    "forward": 0,
+    "jump": 0,
+    "left": 0,
+    "right": 0,
+    "sneak": 0,
+    "sprint": 0
+},
+
+{
+    "attack": 0,
+    "back": 0,
+    "camera": [0,2],
+    "forward": 0,
+    "jump": 0,
+    "left": 0,
+    "right": 0,
+    "sneak": 0,
+    "sprint": 0
+},
+
+{
+    "attack": 0,
+    "back": 0,
+    "camera": [0,-2],
+    "forward": 0,
+    "jump": 0,
+    "left": 0,
+    "right": 0,
+    "sneak": 0,
+    "sprint": 0
+},
+
+{
+    "attack": 0,
+    "back": 0,
+    "camera": [0,0],
+    "forward": 0,
+    "jump": 1,
+    "left": 0,
+    "right": 0,
+    "sneak": 0,
+    "sprint": 0
+},
+
+{
+    "attack": 0,
+    "back": 0,
+    "camera": [0,0],
+    "forward": 1,
+    "jump": 1,
+    "left": 0,
+    "right": 0,
+    "sneak": 0,
+    "sprint": 0
+},
+
+{
+    "attack": 1,
+    "back": 0,
+    "camera": [0,0],
+    "forward": 0,
+    "jump": 0,
+    "left": 0,
+    "right": 0,
+    "sneak": 0,
+    "sprint": 0
+}
+
+]
---
+++ model.py
@@ -0,0 +1,182 @@
+import math
+import random
+import matplotlib
+import matplotlib.pyplot as plt
+from collections import namedtuple, deque
+from itertools import count
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+import gym
+import minerl
+from minerl.data import BufferedBatchIter
+
+# Actions are defined as dictionaries in mineRL. A smaller list of actions is defined separately and these are imported here for simplicity
+from actions import actions 
+
+# Setting up a device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Setting up the replay memory
+Transition = namedtuple('Transition', ('state', 'action', 'reward', 'next_state'))
+
+
+class ReplayMemory:
+
+    def __init__(self, capacity):
+        self.memory = deque([], maxlen=capacity)
+
+    def append(self, *args):
+        """Save a transition"""
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+
+# Defining the model class
+class DQfD(nn.Module):
+
+    def __init__(self, n_observation_feats, n_actions):
+        super(DQfD, self).__init__()
+        self.layer1 = nn.Linear(n_observation_feats, 128)
+        self.layer2 = nn.Linear(128, 128)
+        self.layer3 = nn.Linear(128, n_actions)
+
+    # Called with either one element to determine next action, or a batch
+    # during optimization. Returns tensor([[left0exp,right0exp]...]).
+    def forward(self, x):
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
+        return self.layer3(x)
+
+
+# Defining model hyper-parameters
+# BATCH_SIZE is the number of transitions sampled from the replay buffer
+# GAMMA is the discount factor as mentioned in the previous section
+# EPS is the epsilon greedy exploration probability
+# TAU is the update rate of the target network
+# LR is the learning rate of the AdamW optimizer
+FRAME_STACK = 2
+BATCH_SIZE = 128
+GAMMA = 0.99
+EPS = 0.01
+TAU = 0.005
+LR = 1e-4
+
+
+# Creating the environment (this may take a few minutes) and setting up the data sampling iterator
+env = gym.make('MineRLTreechop-v0')
+data = minerl.data.make('MineRLTreechop-v0')
+iterator = BufferedBatchIter(data)
+gen = iterator.buffered_batch_iter(batch_size=4, num_epochs=1)
+
+
+
+n_observation_feats = 64 * 64 * FRAME_STACK 
+n_actions = 13
+done = False
+
+
+# Defining the Q networks
+policy_net = DQN(n_observations, n_actions).to(device)
+target_net = DQN(n_observations, n_actions).to(device)
+target_net.load_state_dict(policy_net.state_dict())
+
+optimizer = optim.AdamW(policy_net.parameters(), lr=LR, amsgrad=True)
+memory = ReplayMemory(1000)
+
+
+# Defining epsilon greedy action selection
+def select_action(state):
+    sample = random.random()
+    if sample > EPS:
+        with torch.no_grad():
+            return policy_net(state).max(1)[1].view(1, 1)
+    else:
+        return random.choice(actions)
+
+
+def optimize_model(memory):
+    if len(memory) < BATCH_SIZE:
+        return
+    transitions = memory.sample(BATCH_SIZE)
+    batch = Transition(*zip(*transitions))
+
+    # Compute a mask of non-final states and concatenate the batch elements
+    # (a final state would've been the one after which simulation ended)
+    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
+                                          batch.next_state)), device=device, dtype=torch.bool)
+    non_final_next_states = torch.cat([s for s in batch.next_state
+                                                if s is not None])
+    state_batch = torch.cat(batch.state)
+    action_batch = torch.cat(batch.action)
+    reward_batch = torch.cat(batch.reward)
+    state_action_values = policy_net(state_batch).gather(1, action_batch)
+
+    next_state_values = torch.zeros(BATCH_SIZE, device=device)
+    with torch.no_grad():
+        next_state_values[non_final_mask] = target_net(non_final_next_states).max(1)[0]
+    # Compute the expected Q values
+    expected_state_action_values = (next_state_values * GAMMA) + reward_batch
+
+    # Compute Huber loss
+    criterion = nn.SmoothL1Loss()
+    loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
+
+    # Optimize the model
+    optimizer.zero_grad()
+    loss.backward()
+    # In-place gradient clipping
+    torch.nn.utils.clip_grad_value_(policy_net.parameters(), 100)
+    optimizer.step()
+
+
+# Main function
+num_episodes = 100
+num_steps = 10000
+
+for i_episode in range(num_episodes):
+    # Initialize the environment and get it's state
+    state = env.reset()
+
+    for t in range(num_steps):
+        action = select_action(state)
+        observation, reward, done, _ = env.step(action.item())
+        reward = torch.tensor([reward], device=device)
+
+        if done:
+            next_state = None
+        else:
+            next_state = observation
+
+        # Store the transition in memory
+        memory.append(state, action, next_state, reward)
+
+        # Move to the next state
+        state = next_state
+
+        # Perform one step of the optimization (on the policy network)
+        optimize_model()
+
+        # Soft update of the target network's weights
+        # θ′ ← τ θ + (1 −τ )θ′
+        target_net_state_dict = target_net.state_dict()
+        policy_net_state_dict = policy_net.state_dict()
+        for key in policy_net_state_dict:
+            target_net_state_dict[key] = policy_net_state_dict[key]*TAU + target_net_state_dict[key]*(1-TAU)
+        target_net.load_state_dict(target_net_state_dict)
+
+        if done:
+            episode_durations.append(t + 1)
+            break
+
+print('Complete')
+plot_durations(show_result=True)
+plt.ioff()
+plt.show()

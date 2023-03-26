c32816e4ecf06185a45023f69261f7c2cd094465
Anish Diwan <anishhdiwan@gmail.com>
2023-03-26 16:41:27 +0200

    Add action mapping

--- test_minerl/test_action_aggregation.py
+++ test_minerl/test_action_aggregation.py
@@ -8,6 +8,10 @@ data = minerl.data.make('MineRLTreechop-v0')
 iterator = BufferedBatchIter(data)
 demo_replay_memory = iterator.buffered_batch_iter(batch_size=2, num_epochs=1)
 
+basic_actions = {'forward', 'back', 'left', 'right', 'attack', 'jump', 'camera'}
+action_combos = [{'forward', 'left'}, {'forward', 'right'}, {'forward', 'jump'}, {'forward', 'attack'}]
+
+
 def get_aggregate_action(actions, cam_threshold=2.0):
 	actions.pop('sneak')
 	actions.pop('sprint')
@@ -67,16 +71,58 @@ def get_aggregate_action(actions, cam_threshold=2.0):
 
 
 
+def map_aggregate_action(aggregate_action):
+	'''
+	Function to map an aggregate action to one of the agent's available actions 
+	'''
+
+	if len(aggregate_action.keys()) == 0:
+		action = 'noop'
+	elif len(aggregate_action.keys()) == 1:
+		if aggregate_action.keys()[0] in basic_actions:
+			action = aggregate_action.keys()[0]
+	elif len(aggregate_action.keys()) == 2:
+		if set(aggregate_action.keys()) in action_combos:
+			action = set(aggregate_action.keys())
+		else:
+			max_idx = [i for i, x in enumerate(aggregate_action.values()) if x == max(aggregate_action.values())]
+			action = aggregate_action.keys()[random.choice(max_idx)]
+
+	elif len(aggregate_action.keys()) > 2:
+		action_pairs = list(combinations(aggregate_action.keys(), 2))
+		max_occurences = 0
+		action = None
+		for pair in action_pairs:
+			if set(pair) in action_combos:
+				if aggregate_action[pair[0]] + aggregate_action[pair[1]] > max_occurences: 
+					action = set(pair)
+					max_occurences = aggregate_action[pair[0]] + aggregate_action[pair[1]]
+			else:
+				max_idx = [i for i, x in enumerate(aggregate_action.values()) if x == max(aggregate_action.values())]
+				action = aggregate_action.keys()[random.choice(max_idx)]
+
+
+	return action
+
+
+
+
 
 
 for i in range(5):
 	current_states, actions, rewards, next_states, dones = next(demo_replay_memory)
-	print(f'actions: {actions}')
-	print(f'rewards: {rewards}')
-	print('----------')
+	# print(f'actions: {actions}')
+	# print(f'rewards: {rewards}')
+	# print('----------')
 
 	aggregate_reward = np.sum(rewards)
 	aggregate_action = get_aggregate_action(actions)
 	print(f'aggregate action: {aggregate_action}')
 	print(f'aggregate reward: {aggregate_reward}')
 	print('################')
+
+
+	agent_action = map_aggregate_action(aggregate_action)
+	print(agent_action)
+
+

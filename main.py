Fix target net gradients issue in loss definition

main.py
@@ -2,6 +2,7 @@ import torch
 import torch.optim as optim
 import cv2
 import numpy as np
+from tqdm import tqdm
 
 import gym
 import minerl
@@ -98,7 +99,6 @@ total_steps = 0
 
 # Main function
 for i_episode in range(num_episodes):
-
     # Initialize the environment and get it's state
     obs = env.reset()
     # print("Reset Successful")
@@ -110,8 +110,9 @@ for i_episode in range(num_episodes):
     # Metrics
     episode_return = 0
     episode_steps = 0
-
-    for t in range(num_steps):
+    loop = tqdm(range(num_steps))
+    for t in loop:
+        loop.set_description(f"Episode {i_episode} Steps")
         if architecture == "simple":
             action = model.select_action(torch.reshape(torch.tensor(state, dtype=torch.float32), (1,-1)), EPS, policy_net)
         elif architecture == "duelling_net":

model.py
@@ -137,14 +137,18 @@ class DQfD_Loss(nn.Module):
 
     def forward(self, policy_net, target_net, states, actions, rewards, next_states, dones, GAMMA, large_margin=True):
         # 1-step TD loss
-        targets = rewards + GAMMA * torch.max(target_net(next_states), dim=1).values
+        with torch.no_grad():
+            next_state_max = torch.max(target_net(next_states), dim=1).values
+
+        targets = rewards + GAMMA * next_state_max # torch.max(target_net(next_states), dim=1).values
         values = policy_net(states).gather(1,actions.view(-1,1)).view(-1,)
 
         if large_margin == True:
             # large margin loss
             lm1 = torch.max(policy_net(states), dim=1).values # Q value of the agent's actions as per its current policy
             lm2 = torch.eq(torch.argmax(policy_net(states), dim=1), actions).float() # Margin function: 0 if agent's action is the same as the expert's action 1 otherwise
-            lm3 = target_net(states).gather(1, actions.view(-1,1)).view(-1,) # Q value of the expert's action as per the target net (similar to why we use a frozen target)
+            with torch.no_grad():
+                lm3 = target_net(states).gather(1, actions.view(-1,1)).view(-1,) # Q value of the expert's action as per the target net (similar to why we use a frozen target)
 
             large_margin_loss = torch.mean(lm1+lm2+lm3)        
 